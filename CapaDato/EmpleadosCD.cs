be4f880cf607c280f7a04125d44bac9cb5cf8dbc
Marcelo Leoncio Diaz Sifuentes <Choscreator1212@gmail.com>
2024-09-09 02:00:26 -0500

    DataGridView de Empleados Agregado

--- CapaDato/EmpleadosCD.cs
+++ CapaDato/EmpleadosCD.cs
@@ -29,5 +29,41 @@ namespace CapaDato
                 return dt;
             }
         }
+        public DataTable ObtenerInformacionEmpleados()
+        {
+            DataTable dt = new DataTable();
+            using (SqlConnection cnx = ConexionCD.sqlConnection())
+            {
+                try
+                {
+                    string query = @"
+                SELECT e.Nombre, e.Apellido1, e.Apellido2, e.DNI, e.Telefono, e.CorreoElectronico, e.FechaNacimiento, e.Direccion, e.Distrito,
+                       dl.Cargo, dl.Area, dl.EstadoLaboral, dl.Nombre_Supervisor,
+                       da.UniversidadInstituto, da.Carrera
+                FROM Empleados e
+                LEFT JOIN DatosLaborales dl ON e.ID = dl.ID_Empleado
+                LEFT JOIN DatosAcademicos da ON e.ID = da.ID_Empleado";
+
+                    SqlCommand cmd = new SqlCommand(query, cnx);
+                    SqlDataAdapter da = new SqlDataAdapter(cmd);
+                    da.Fill(dt);
+                }
+                catch (Exception ex)
+                {
+                    throw new Exception("Error al obtener la información: " + ex.Message);
+                }
+                finally
+                {
+                    if (cnx.State == ConnectionState.Open)
+                    {
+                        cnx.Close();
+                    }
+                }
+            }
+            return dt;
+        }
+
+
+
     }
 }
--- CapaPresentacion/Administrador.cs
+++ CapaPresentacion/Administrador.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using CapaNegocio;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -19,13 +20,37 @@ namespace CapaPresentacion
 
         private void button1_Click(object sender, EventArgs e)
         {
-            
+
         }
 
         private void btnEditar_Click(object sender, EventArgs e)
         {
             Editar Editar = new Editar();
             Editar.Show();
+        }
+
+        private void btnActualizar_Click(object sender, EventArgs e)
+        {
+            {
+                try
+                {
+                    DataTable dt = EmpleadoCN.ObtenerInformacionEmpleados();
+                    if (dt != null && dt.Rows.Count > 0)
+                    {
+                        dataGridView.DataSource = dt;
+                    }
+                    else
+                    {
+                        dataGridView.DataSource = null;
+                        MessageBox.Show("No se encontraron datos para mostrar.");
+                    }
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show($"Error al obtener los datos: {ex.Message}");
+                }
+            }
+
         }
     }
 }
